2e10f2cc404f962559a036ac022389b5b30111ad
Idam <idam.elazhar@gmail.com>
2025-05-26 13:48:17 +0700

    Lots.

--- Firebase/progress.js
+++ Firebase/progress.js
@@ -1,14 +1,13 @@
-import { doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getDatabase, ref, set, get } from "https://www.gstatic.com/firebasejs/11.8.0/firebase-database.js";
+import { app } from '/Firebase/firebase.js';
 
-const db = window.db;
+const db = getDatabase(app);
 
 export async function saveProgress(userId, progressData) {
-  const userRef = doc(db, "users", userId);
-  await setDoc(userRef, { progress: progressData }, { merge: true });
+  await set(ref(db, 'users/' + userId + '/progress'), progressData);
 }
 
 export async function loadProgress(userId) {
-  const userRef = doc(db, "users", userId);
-  const docSnap = await getDoc(userRef);
-  return docSnap.exists() ? docSnap.data().progress : null;
+  const snapshot = await get(ref(db, 'users/' + userId + '/progress'));
+  return snapshot.exists() ? snapshot.val() : null;
 }
